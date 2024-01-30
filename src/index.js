4190fbcd944a957ca3eb1a0e585781f0da32061f
JipsonFrancis <francis.boy.jipson.jnr@gmail.com>
2024-01-31 01:48:25 +0200

    UI card props

--- src/index.js
+++ src/index.js
@@ -8,27 +8,28 @@ function App()
 {
     return (
         <div className="row">
-            <Project />
-            <Project />
+            <Project name={'GradeAssessment'} styling={{backgroundColor: 'rgba(165, 96, 247, 0.43)', color: 'rgb(85, 27, 177)'}} tech={['Analytics', 'php']}/>
+            <Project name={'Potefolio'} styling={{backgroundColor: 'rgba(165, 96, 247, 0.43)', color: 'rgb(85, 27, 177)'}} tech={['Analytics', 'Web3', 'React']}/>
         </div>
     )
 }
 
-function Project()
+function Project(props)
 {
     return (
         <article className="article-wrapper">
             <div className="rounded-lg container-project"></div>
             <div className="project-info">
                 <div className="flex-pr">
-                    <div className="project-title text-nowrap">Project</div>
+                    <div className="project-title text-nowrap">{props.name}</div>
                     <div className="project-hover">
                       <svg style={{color: "black"}} xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" color="black" stroke-linejoin="round" stroke-linecap="round" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke="currentColor"><line y2="12" x2="19" y1="12" x1="5"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
                     </div>
                 </div>
                 <div className="types">
-                    <span style={{backgroundColor: 'rgba(165, 96, 247, 0.43)', color: 'rgb(85, 27, 177)'}} className="project-type">• Analytics</span>
-                    <span className="project-type">• Dashboards</span>
+                    <span style={props.styling} className="project-type">• {props.tech[0]}</span>
+                    <span className="project-type">• {props.tech[1]}</span>
+                    <span className="project-type">• {props.tech[2]}</span>
                 </div>
             </div>
         </article>
